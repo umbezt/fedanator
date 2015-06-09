af0e21104e6d6c4c0a5c46e461d6051427203f52
Oscar Mwanandimai <oscar.mwana@gmail.com>
2015-06-09 12:26:43 +0200

    removed unwanted plugins

--- gulpfile.js
+++ gulpfile.js
@@ -2,38 +2,24 @@
 var gulp = require('gulp');
 
 // include plug-ins
-var jshint = require('gulp-jshint');
 var changed = require('gulp-changed');
 var imagemin = require('gulp-imagemin');
 var concat = require('gulp-concat');
-var stripDebug = require('gulp-strip-debug');
 var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
-var minifyCSS = require('gulp-minify-css');
-var notify = require("gulp-notify");
 var watch = require("gulp-watch");
 
 // Gulp copy bootstrap, jquery
 gulp.task('vendors', function () {
     gulp.src('./bower_components/bootstrap/dist/css/bootstrap.css')
         .pipe(changed('./assets/css/'))
-        .pipe(gulp.dest('./assets/css/'))
-        .pipe(notify("Bootstrap CSS imported"));
+        .pipe(gulp.dest('./assets/css/'));
     gulp.src('./bower_components/bootstrap/dist/js/bootstrap.js')
         .pipe(changed('./assets/js/'))
-        .pipe(gulp.dest('./assets/js/'))
-        .pipe(notify("Bootstrap JS imported"));
+        .pipe(gulp.dest('./assets/js/'));
     gulp.src('./bower_components/jquery/dist/jquery.js')
         .pipe(changed('./assets/js/'))
-        .pipe(gulp.dest('./assets/js/'))
-        .pipe(notify("jQuery imported"));
-});
-
-// JS hint task on dev js files
-gulp.task('jshint', function () {
-    return gulp.src('./build/js/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+        .pipe(gulp.dest('./assets/js/'));
 });
 
 // minify new images
@@ -42,47 +28,33 @@ gulp.task('images', function () {
     return gulp.src(['./build/img/**/*.png', './build/img/**/*.jpg', './build/img/**/*.jpeg', './build/img/**/*.gif'])
         .pipe(changed('./assets/img'))
         .pipe(imagemin())
-        .pipe(gulp.dest('./assets/img'))
-        .pipe(notify({message: 'Images compressed and copied to the assets folder.'}));
+        .pipe(gulp.dest('./assets/img'));
 });
 
 // JS concat, strip debugging and minify
 gulp.task('scripts', function () {
     return gulp.src('./build/js/*.js')
         .pipe(concat('app.js'))
-        .pipe(stripDebug())
         .pipe(uglify())
-        .pipe(gulp.dest('./assets/js/'))
-        .pipe(notify({message: 'Javascript files have been stripped of debug code, concatenated and minified.'}));
+        .pipe(gulp.dest('./assets/js/'));
 });
 
 // run sass
 gulp.task('sass', function () {
     return gulp.src('./build/sass/*.scss')
         .pipe(sass(
-            {onError: notify.onError('<%= error.message %>')},
             {style: 'compressed'}
         ))
-        .pipe(gulp.dest('./assets/css/'))
-        .pipe(notify({message: 'Sass file compiled.'}));
-});
-
-// CSS concat, auto-prefix and minify
-gulp.task('styles', function () {
-    return gulp.src(['./build/css/*.css'])
-        .pipe(concat('app.css'))
-        .pipe(minifyCSS())
-        .pipe(gulp.dest('./build/css/'))
-        .pipe(notify({message: 'Styles have been concatenated and minified.', onLast: true}));
+        .pipe(gulp.dest('./assets/css/'));
 });
 
 // gulp watch
 gulp.task('watch', function () {
 
     // watch for JS changes
-    gulp.watch('./build/js/*.js', ['jshint', 'scripts']);
+    gulp.watch('./build/js/*.js', ['scripts']);
     watch('./build/js/*.js', function(){
-        gulp.start(['jshint', 'scripts']);
+        gulp.start(['scripts']);
     });
 
     // watch for sass changes
